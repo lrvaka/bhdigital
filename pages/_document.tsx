3b13f9dfdb4b7166d3c47cff2d306d7320a96138
lrvaka <lukevakasisikakala@gmail.com>
2022-12-08 02:22:18 -0500

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/vks2ayv.css" />
+        <link rel="icon" href="/favicon.ico" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link
           rel="stylesheet"
           href="https://use.typekit.net/dwi3esn.css"
