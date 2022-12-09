43147b045ece7497e207a01233e043ddf4a8aebc
lrvaka <lukevakasisikakala@gmail.com>
2022-12-09 05:00:09 -0500

    remove navbar from landing page

--- pages/landing-page.tsx
+++ pages/landing-page.tsx
@@ -46,14 +46,7 @@ const LandingPage: NextPage = () => {
                 <path d="M190.06 378.7c11.26 3.23 19.6-3.73 19.6-17.84 0-16.67-11.63-36.95-25.98-45.29L53.79 240.08v59.1l129.89 75.49c2.15 1.25 4.27 2.61 6.38 4.03ZM183.68 436.69 53.79 361.2v60.38l129.89 75.49c14.35 8.34 25.98 1.58 25.98-15.09s-11.63-36.95-25.98-45.29ZM314.8 271.61l155.4-89.72v120l-155.4 89.72v-120zM314.8 451.61l155.4-89.72v120l-155.4 89.72v-120z" />
               </svg>
             </div>
-            <nav className="list-none flex gap-10 text-2xl self-center justify-center items-center">
-              <a
-                href="#form"
-                className="inline-block rounded-lg bg-rose-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-rose-600 hover:bg-rose-700 hover:ring-rose-700"
-              >
-                Get in touch
-              </a>
-            </nav>
+            
           </header>
           <HeroBackground landingPage />
           <div className=" pt-20">
