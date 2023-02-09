5ce0517532762ed3e3fddc2a4a6bbecc58968d15
lrvaka <lukevakasisikakala@gmail.com>
2023-02-08 23:13:08 -0500

    check out

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -96,5 +96,9 @@ export const getStaticProps = async ({ params }) => {
     props: {
       post: post,
     },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every second
+    revalidate: 1, // In seconds
   };
 };
