85125b361c158f2b80157b558959c3c766367632
lrvaka <lukevakasisikakala@gmail.com>
2023-03-12 00:12:11 -0500

    revalidate ISR from 1 to 10 sec

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -48,7 +48,7 @@ export const getStaticProps = async () => {
     },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
-    // - At most once every second
-    revalidate: 1, // In seconds
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
   };
 };
